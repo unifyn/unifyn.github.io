aad0ebcbe8e0d55ed0a53207132cb17fa6be4cd5
unifyn <brian-mb@live.com>
2018-06-08 15:53:25 +1200

    edit

--- offline.js
+++ offline.js
@@ -1,12 +1,11 @@
 ﻿{
-	"version": 1528429466,
+	"version": 1528429984,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-2.1.1.min.js",
 		"offlineClient.js",
 		"images/tiledbackground.png",
-		"images/sprite-sheet0.png",
 		"images/tiledbackground2.png",
 		"icon-16.png",
 		"icon-32.png",
